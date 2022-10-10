57470bab7b1e4eb3c0d3e19a5f7ad193fe3830f5
aleksa-stajcic <aleksa@byteout.com>
2022-10-10 13:57:40 +0200

    format code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
 use clap::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser)]
 struct Args {
-    #[arg(short='c', long = "character")]
+    #[arg(short = 'c', long = "character")]
     character: char,
     // #[arg(short='c', long = "character")]
     // character: Option<char>,
@@ -12,42 +12,44 @@ struct Args {
 }
 
 fn main() {
-    let map = HashMap::<char, String>::from(
-        [
-            (' ', String::from("%20")),
-            ('!', String::from("%21")),
-            ('#', String::from("%23")),
-            ('$', String::from("%24")),
-            ('%', String::from("%25")),
-            ('&', String::from("%26")),
-            ('\'', String::from("%27")),
-            ('(', String::from("%28")),
-            (')', String::from("%29")),
-            ('*', String::from("%2A")),
-            ('+', String::from("%2B")),
-            (',', String::from("%2C")),
-            ('/', String::from("%2F")),
-            (':', String::from("%3A")),
-            (';', String::from("%3B")),
-            ('=', String::from("%3D")),
-            ('?', String::from("%3F")),
-            ('@', String::from("%40")),
-            ('[', String::from("%5B")),
-            (']', String::from("%5D")),
-        ]
-    );
+    let map = HashMap::<char, String>::from([
+        (' ', String::from("%20")),
+        ('!', String::from("%21")),
+        ('#', String::from("%23")),
+        ('$', String::from("%24")),
+        ('%', String::from("%25")),
+        ('&', String::from("%26")),
+        ('\'', String::from("%27")),
+        ('(', String::from("%28")),
+        (')', String::from("%29")),
+        ('*', String::from("%2A")),
+        ('+', String::from("%2B")),
+        (',', String::from("%2C")),
+        ('/', String::from("%2F")),
+        (':', String::from("%3A")),
+        (';', String::from("%3B")),
+        ('=', String::from("%3D")),
+        ('?', String::from("%3F")),
+        ('@', String::from("%40")),
+        ('[', String::from("%5B")),
+        (']', String::from("%5D")),
+    ]);
 
     let args = Args::parse();
 
     let ch = args.character;
     match map.get(&ch) {
-        Some(e) => {  println!("`{}` => `{}`", ch, e); },
-        None => { panic!("Character `{}` doesn't get encoded.", ch) }
+        Some(e) => {
+            println!("`{}` => `{}`", ch, e);
+        }
+        None => {
+            panic!("Character `{}` doesn't get encoded.", ch)
+        }
     }
 
     // &todo: handle not having entries in map
     // match args.character {
-    //     Some(c) => { 
+    //     Some(c) => {
     //         match map.get(&c) {
     //             Some(e) => { println!("`{}` => `{}`", c, e); },
     //             None => { panic!("Character `{}` doesn't get encoded.", c) } // &todo figure this shit out
